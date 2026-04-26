b37de97596c19f3eb4ea5c5a774e844e8519db49
HassanMohamed-cs <hassanashour639@gmail.com>
2026-04-26 15:21:34 +0300

    Finished

--- image_segment.py
+++ image_segment.py
@@ -2,16 +2,17 @@ import numpy as np
 import cv2 as cv
 from typing import Tuple, Dict
 from numpy.typing import ArrayLike
+from splitmerge import split_and_merge_segmentation
+import dlib as dl
+import os
 
 def SegmentHand(Img: ArrayLike) -> ArrayLike:
-    """
-    Parameters:
-        Img: is the original hand image on any background
-
-    Returns:
-        A NumPy array of the binary image that contains the hand mask only
-    """
-    pass
+    hand_mask = split_and_merge_segmentation(Img)
+    kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (17, 17))
+    hand_mask = cv.morphologyEx(hand_mask, cv.MORPH_CLOSE, kernel)
+    kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (5, 5))
+    hand_mask = cv.morphologyEx(hand_mask, cv.MORPH_OPEN, kernel)
+    return hand_mask
 
 
 def DetectFaces(Img, OutPath) -> Tuple[Dict, Dict]:
@@ -26,4 +27,36 @@ def DetectFaces(Img, OutPath) -> Tuple[Dict, Dict]:
                         the same size AND marking each of the left eye and right eye
             - Eyes: returned struct containing all resized faces after detecting and marking each of the left eye and right eye
     """
-    pass
+    detector = dl.get_frontal_face_detector()
+    predictor = dl.shape_predictor("../../Model/shape_predictor_68_face_landmarks.dat")
+    kernel = np.array([[0,-1,0],
+                      [-1,5,-1],
+                    [0,-1,0]])
+    Img = cv.filter2D(Img, -1, kernel)
+    img_gray = cv.cvtColor(Img, cv.COLOR_BGR2GRAY)
+    faces = detector(img_gray, 2)
+    Faces = []
+    Eyes = []
+    os.makedirs(OutPath, exist_ok=True)
+    for i,face in enumerate(faces):
+        x, y, w, h = face.left(), face.top(), face.width(), face.height()
+        face_img = Img[y:y+h, x:x+w]
+        face_img = cv.resize(face_img, (128, 128))
+        landmarks = predictor(img_gray, face)
+        scale_x = 128 / w
+        scale_y = 128 / h
+        right_eye = [(landmarks.part(i).x, landmarks.part(i).y) for i in range(36,42)]
+        left_eye = [(landmarks.part(i).x, landmarks.part(i).y) for i in range(42,48)]
+        for (ex, ey) in left_eye:
+            ex_rel = int((ex - x) * scale_x)
+            ey_rel = int((ey - y) * scale_y)
+            cv.circle(face_img, (ex_rel, ey_rel), 2, (0, 255, 0), 1)
+        for (ex, ey) in right_eye:
+            ex_rel = int((ex - x) * scale_x)
+            ey_rel = int((ey - y) * scale_y)
+            cv.circle(face_img, (ex_rel, ey_rel), 2, (255,0, 0), 1)
+        out_file = os.path.join(OutPath, f"{i}.png")
+        cv.imwrite(out_file, face_img)
+        Faces.append(face_img)
+        Eyes.append({"left": left_eye, "right": right_eye})
+    return Faces, Eyes
--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 import matplotlib.pyplot as plt
 import cv2 as cv
-import image_segment
-from splitmerge import split_and_merge_segmentation
+from image_segment import SegmentHand, DetectFaces
 def Problem_1():
-    #case_choice = int(input("Test Case #: "))
-
-    # Open input image based on Case Choice
-    input_image = cv.imread("../Imgs/Prob1/Sample/hand.jpg")
-    output_image = split_and_merge_segmentation(input_image)
+    case_choice = int(input("Test Case #: "))
+    input_image = cv.imread(f"../../Imgs/Prob1/{case_choice}.jpg")
+    output_image = SegmentHand(input_image)
     input_image_rgb = cv.cvtColor(input_image, cv.COLOR_BGR2RGB)
-    ## Plot both images side-by-side
     fig, ax = plt.subplots(1, 2)
     ax[0].imshow(input_image_rgb)
     ax[1].imshow(output_image, cmap='gray')
@@ -20,9 +16,27 @@ def ApplyAll(inputPaths_File, outputPath):
 
 def Problem_2():
     case_choice = int(input("Test Case #: "))
-
-    # Your Code
+    image = cv.imread(f"../../Imgs/Prob2/{case_choice}.jpg")
+    Faces, Eyes = DetectFaces(image, "../../Imgs/Prob2/Sample/")
+    image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
+    print(Eyes)
+    fig = plt.figure(figsize=(12, 6))
+    plt.subplot(1, len(Faces) + 1, 1)
+    plt.imshow(image)
+    plt.axis('off')
+    for i, face in enumerate(Faces):
+        face = cv.cvtColor(face, cv.COLOR_BGR2RGB)
+        plt.subplot(1, len(Faces) + 1, i + 2)
+        plt.imshow(face)
+        plt.axis('off')
+    red_patch = plt.Line2D([0], [0], marker='o', color='w', label='Right Eye',
+                           markerfacecolor='blue', markersize=8)
+    green_patch = plt.Line2D([0], [0], marker='o', color='w', label='Left Eye',
+                             markerfacecolor='green', markersize=8)
+    fig.legend(handles=[red_patch, green_patch],
+               loc='lower center', ncol=2)
+    plt.show()
 
 
 Problem_1()
-#Problem_2()
+Problem_2()
